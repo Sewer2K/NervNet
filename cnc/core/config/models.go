7691c1977795f9ef39db3920e744b0702f782bc8
Sewer2K <stormjameskillian3@gmail.com>
2026-04-27 23:49:11 +0200

    Fix Discord config panic: add custom StringSlice type in Go to accept both JSON numbers and strings for admin/channel IDs

--- cnc/core/config/models.go
+++ cnc/core/config/models.go
@@ -1,6 +1,34 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
+
 var Config *ConfigModel
+
+// StringSlice is a []string that can unmarshal from both JSON strings and numbers
+type StringSlice []string
+
+func (s *StringSlice) UnmarshalJSON(data []byte) error {
+	var raw []interface{}
+	if err := json.Unmarshal(data, &raw); err != nil {
+		return err
+	}
+	*s = make([]string, len(raw))
+	for i, v := range raw {
+		switch val := v.(type) {
+		case string:
+			(*s)[i] = val
+		case float64:
+			(*s)[i] = strconv.Itoa(int(val))
+		default:
+			return fmt.Errorf("cannot unmarshal %T into string", v)
+		}
+	}
+	return nil
+}
 
 type ConfigModel struct {
 	Server struct {
@@ -28,12 +56,12 @@ type ConfigModel struct {
 		Admins   []string `json:"admins"`
 	} `json:"telegram"`
 	Discord struct {
-		Enabled             bool     `json:"enabled"`
-		BotToken            string   `json:"botToken"`
-		Prefix              string   `json:"prefix"`
-		Admins              []string `json:"admins"`
-		AllowedChannels     []string `json:"allowedChannels"`
-		NotificationChannel string   `json:"notificationChannel"`
+		Enabled             bool        `json:"enabled"`
+		BotToken            string      `json:"botToken"`
+		Prefix              string      `json:"prefix"`
+		Admins              StringSlice `json:"admins"`
+		AllowedChannels     StringSlice `json:"allowedChannels"`
+		NotificationChannel string      `json:"notificationChannel"`
 	} `json:"discord"`
 	Queue struct {
 		QueuedMessage    string `json:"queuedMessage"`
